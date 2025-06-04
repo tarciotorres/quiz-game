84e78d1eb80f71e493b0a40a9a1dfba3f7e4c8eb
tarciotorres <tarciotmelo2009@gmail.com>
2025-06-04 08:30:07 -0300

    update

--- logica.js
+++ logica.js
@@ -1,3 +1,54 @@
+
+listaPerguntas = [
+  {
+    pergunta: "Qual o maior ser vivo do mundo?",
+    opcoes: [
+      "Baleia Azul",
+      "Cogumelo de mel",
+      "Elefante",
+      "Girafa"
+    ],
+    correta : 1
+  },
+  
+  {
+    pergunta: "Qual a primeira capital do Brasil?",
+    opcoes: [
+      "Taquara",
+      "Salvador",
+      "Brasilia",
+      "Rio de Janeiro"
+    ],
+    correta : 1
+  },
+
+  {
+    pergunta: "Em que ano o Brasil se tornou independente?",
+    opcoes: [
+      "1980",
+      "1500",
+      "1237",
+      "1822"
+    ],
+    correta : 3
+  },
+
+  {
+    pergunta:"Que ano o Flamengo foi campeão mundial?",
+    opcoes: [
+      "2002",
+      "2019",
+      "1981",
+      "2025"
+    ],
+    correta : 2
+  },
+]
+  
+Pergunta = null
+
+
+
 btnIniciar = document .querySelector(".Iniciar")
 
 btnIniciar.addEventListener("click" , Iniciarjogo)
@@ -8,4 +59,12 @@ function Iniciarjogo() {
   
   telaPergunta = document.querySelector(".tela-pergunta")
   telaPergunta.style.display = "block"
+  
+  mostrarPergunta()
+}
+
+function mostrarPergunta() {
+  pergunta = listaPerguntas [0]
+  
+  console.log(pergunta)
 }
